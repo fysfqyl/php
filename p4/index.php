bb7611c79c2de5aceb7790af467b75f32d61d944
fysfqyl <576828651agu@gmail.com>
2022-04-11 18:57:57 +0800

    master

--- p4/index.php
+++ p4/index.php
@@ -1,6 +1,6 @@
 <?php
     header('Content-type:text/html;charset=utf-8');
-
+//1、定义一个类
     class Student {
         public $name;
         public $age;
@@ -8,10 +8,13 @@
             echo '我是一个好人';
         }
     }
+//2、实例化对象
     $stu1 = new Student;
     $stu1 -> speak();
+//3、给对象赋值
     $stu1 -> name = '张三';
     $stu1 -> age = '18';
+//4、打印对象
     echo '<pre>';
     var_dump($stu1);
     echo '</pre>';
